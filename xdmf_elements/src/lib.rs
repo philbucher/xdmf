b893b29473270f6c588f848b603e5d3417ee2a9e
Philipp Bucher <philippbucher@gmx.de>
2025-06-21 20:28:46 +0200

    top-level xdmf elements improvements

--- xdmf_elements/src/lib.rs
+++ xdmf_elements/src/lib.rs
@@ -29,7 +29,13 @@ impl Xdmf {
     }
 }
 
-#[derive(Debug, Serialize)]
+impl Default for Xdmf {
+    fn default() -> Self {
+        Self::new(Domain::default())
+    }
+}
+
+#[derive(Debug, Default, Serialize)]
 pub struct Domain {
     #[serde(rename = "Grid")]
     pub grids: Vec<Grid>,
@@ -38,5 +44,62 @@ pub struct Domain {
 impl Domain {
     pub fn new(grid: Grid) -> Self {
         Self { grids: vec![grid] }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_xdmf_new() {
+        let domain = Domain::default();
+        let xdmf = Xdmf::new(domain);
+
+        assert_eq!(xdmf.version, "3.0");
+        assert_eq!(xdmf.domains.len(), 1);
+    }
+
+    #[test]
+    fn test_xdmf_default() {
+        let xdmf = Xdmf::default();
+
+        assert_eq!(xdmf.version, "3.0");
+        assert_eq!(xdmf.domains.len(), 1);
+    }
+
+    #[test]
+    fn test_domain_new() {
+        let grid = Grid::new_uniform(
+            "test_grid",
+            geometry::Geometry {
+                geometry_type: geometry::GeometryType::XYZ,
+                data_item: data_item::DataItem {
+                    dimensions: dimensions::Dimensions(vec![3]),
+                    data: "1.0 2.0 3.0".to_string(),
+                    number_type: data_item::NumberType::Float,
+                    ..Default::default()
+                },
+            },
+            topology::Topology {
+                topology_type: topology::TopologyType::Triangle,
+                number_of_elements: "1".to_string(),
+                data_item: data_item::DataItem {
+                    dimensions: dimensions::Dimensions(vec![3]),
+                    number_type: data_item::NumberType::Int,
+                    data: "0 1 2".to_string(),
+                    ..Default::default()
+                },
+            },
+        );
+        let domain = Domain::new(grid);
+
+        assert_eq!(domain.grids.len(), 1);
+    }
+
+    #[test]
+    fn test_domain_default() {
+        let domain = Domain::default();
+        assert!(domain.grids.is_empty());
     }
 }
