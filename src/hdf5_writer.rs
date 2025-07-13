30627fa6c260a901d9eccd231c24c8d6c6e94a53
Philipp Bucher <philippbucher@gmx.de>
2025-07-13 15:36:47 +0200

    implement write_mesh for MultipleFilesHdf5Writer

--- src/hdf5_writer.rs
+++ src/hdf5_writer.rs
@@ -49,7 +49,6 @@ impl DataWriter for SingleFileHdf5Writer {
 }
 
 pub(crate) struct MultipleFilesHdf5Writer {
-    #[allow(dead_code)] // remove this, only temp to silence clippy
     h5_files_dir: PathBuf,
 }
 
@@ -68,9 +67,30 @@ impl DataWriter for MultipleFilesHdf5Writer {
         Format::HDF
     }
 
-    fn write_mesh(&mut self, _points: &[f64], _cells: &[u64]) -> IoResult<(String, String)> {
-        let _file_name = self.h5_files_dir.join("mesh.h5");
-        unimplemented!()
+    fn write_mesh(&mut self, points: &[f64], cells: &[u64]) -> IoResult<(String, String)> {
+        let file_name = self.h5_files_dir.join("mesh.h5");
+        let h5_file = H5File::create(&file_name).map_err(std::io::Error::other)?;
+
+        h5_file
+            .new_dataset::<f64>()
+            .shape(points.len())
+            .create("points")
+            .map_err(std::io::Error::other)?
+            .write(points)
+            .map_err(std::io::Error::other)?;
+
+        h5_file
+            .new_dataset::<u64>()
+            .shape(cells.len())
+            .create("cells")
+            .map_err(std::io::Error::other)?
+            .write(cells)
+            .map_err(std::io::Error::other)?;
+
+        Ok((
+            file_name.join("points").to_string_lossy().to_string(),
+            file_name.join("cells").to_string_lossy().to_string(),
+        ))
     }
 
     #[cfg(feature = "unstable-submesh-api")]
@@ -91,10 +111,12 @@ impl DataWriter for MultipleFilesHdf5Writer {
 
 #[cfg(test)]
 mod tests {
+    use float_cmp::assert_approx_eq;
+
     use super::*;
 
     #[test]
-    fn test_mutliple_files_hdf5_writer_create_dir() {
+    fn test_mutliple_files_hdf5_writer_new() {
         let tmp_dir = temp_dir::TempDir::new().unwrap();
         let file_name = tmp_dir.path().join("test.xdmf");
         let writer = MultipleFilesHdf5Writer::new(&file_name).unwrap();
@@ -102,5 +124,36 @@ mod tests {
         assert_eq!(writer.h5_files_dir, exp_dir_name);
         assert!(writer.h5_files_dir.exists());
         assert!(writer.h5_files_dir.is_dir());
+    }
+
+    #[test]
+    fn test_mutliple_files_hdf5_writer_write_mesh() {
+        let tmp_dir = temp_dir::TempDir::new().unwrap();
+        let file_name = tmp_dir.path().join("test.xdmf");
+        let mut writer = MultipleFilesHdf5Writer::new(file_name).unwrap();
+        let mesh_file = writer.h5_files_dir.join("mesh.h5");
+        assert!(!mesh_file.exists());
+
+        let points = vec![0.0, 1.0, 2.0];
+        let cells = vec![0, 1, 2];
+        let (points_path, cells_path) = writer.write_mesh(&points, &cells).unwrap();
+        assert!(mesh_file.exists());
+
+        assert_eq!(
+            points_path,
+            mesh_file.join("points").to_string_lossy().to_string()
+        );
+        assert_eq!(
+            cells_path,
+            mesh_file.join("cells").to_string_lossy().to_string()
+        );
+
+        // read back the data to verify
+        let h5_file = H5File::open(&mesh_file).unwrap();
+        let points_data: Vec<f64> = h5_file.dataset("points").unwrap().read().unwrap().to_vec();
+        let cells_data: Vec<u64> = h5_file.dataset("cells").unwrap().read().unwrap().to_vec();
+
+        assert_approx_eq!(&[f64], &points, &points_data);
+        assert_eq!(&cells, &cells_data);
     }
 }
